a9b6bd1900492f603a874517a5f417730b98c092
nicoandresr <nicoandres.rodriguez@gmail.com>
2017-11-04 11:08:31 -0500

    Fix some bugs when the string to capitalize is undefinded or a number

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const getLowerCaseString = string => (string || '').toString().toLowerCase();
+
 module.exports = function (string) {
-  string = string.toLowerCase();
+  string = getLowerCaseString(string);
   return string.charAt(0).toUpperCase() + string.substring(1);
 }
 
 module.exports.words = function (string) {
-  return string.toLowerCase().replace(/(^|[^a-zA-Z\u00C0-\u017F'])([a-zA-Z\u00C0-\u017F])/g, function (m) {
-    string = string.toLowerCase();
+  return getLowerCaseString(string).replace(/(^|[^a-zA-Z\u00C0-\u017F'])([a-zA-Z\u00C0-\u017F])/g, function (m) {
     return m.toUpperCase()
-  })
+  });
 }
+
